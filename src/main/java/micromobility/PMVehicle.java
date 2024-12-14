fd506f60307f151e7111a1fdfe9fe351a8cae835
PauOS004 <pau.ortiz.sanchez@gmail.com>
2024-12-14 15:57:35 +0100

    TODO DONE!
    new methods:
    - getState
    - setNotAvailb
    - setUnderWay
    -setAvailb
    
    Co-Authored-By: GerardPueyo <189444787+GerardPueyo@users.noreply.github.com>

--- src/main/java/micromobility/PMVehicle.java
+++ src/main/java/micromobility/PMVehicle.java
@@ -4,12 +4,12 @@ import data.GeographicPoint;
 
 public class PMVehicle {
     private String vehicleID; // Asumiendo que puede ser un identificador único
-
+    private PMVState state;
     private GeographicPoint location;
 
-    public PMVehicle(String vehicleID, GeographicPoint location) {
+    public PMVehicle(String vehicleID, PMVState state, GeographicPoint location) {
         this.vehicleID = vehicleID;
-
+        this.state = state;
         this.location = location;
     }
 
@@ -17,13 +17,25 @@ public class PMVehicle {
         return vehicleID;
     }
 
-
+    public PMVState getState() {
+        return state;
+    }
 
     public GeographicPoint getLocation() {
         return location;
     }
 
+    public void setNotAvailb() {
+        this.state = PMVState.NotAvailable;
+    }
 
+    public void setUnderWay() {
+        this.state = PMVState.UnderWay;
+    }
+
+    public void setAvailb() {
+        this.state = PMVState.Available;
+    }
 
     public void setLocation(GeographicPoint gP) {
         this.location = gP;
@@ -33,6 +45,7 @@ public class PMVehicle {
     public String toString() {
         return "PMVehicle{" +
                 "vehicleID='" + vehicleID + '\'' +
+                ", state=" + state +
                 ", location=" + location +
                 '}';
     }
